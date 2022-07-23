85ac9b8cfea35e3d09c20b1edc1b5f486bcd85e1
svanonline <101625280+svanonline@users.noreply.github.com>
2022-07-23 14:20:48 +0300

    update

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,23 +1,59 @@
+import datetime as dt
+
 from rest_framework import serializers
 
-from .models import Achievement, AchievementCat, Cat, Owner
+from djoser.serializers import UserSerializer
+
+from .models import Achievement, AchievementCat, Cat, Owner, CHOICES
+
+import webcolors
+
+
+class Hex2NameColor(serializers.Field):
+    # При чтении данных ничего не меняем - просто возвращаем как есть
+    def to_representation(self, value):
+        return value
+    # При записи код цвета конвертируется в его название
+
+    def to_internal_value(self, data):
+        # Доверяй, но проверяй
+        try:
+            # Если имя цвета существует, то конвертируем код в название
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            # Иначе возвращаем ошибку
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        # Возвращаем данные в новом формате
+        return data
 
 
 class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
 
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achievement_name')
+
+
+class CatListSerializer(serializers.ModelSerializer):
+    color = serializers.ChoiceField(choices=CHOICES)
+
+    class Meta:
+        model = Cat
+        fields = ('id', 'name', 'color')
 
 
 class CatSerializer(serializers.ModelSerializer):
     owner = serializers.StringRelatedField(read_only=True)
     # Переопределяем поле achievements
     achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    color = Hex2NameColor(choices=CHOICES)  # Вот он - наш собственный тип поля
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements') 
+        fields = ('id', 'name', 'color', 'birth_year',
+                  'owner', 'achievements', 'age')
 
     def create(self, validated_data):
         # Если в исходном запросе не было поля achievements
@@ -25,6 +61,9 @@ class CatSerializer(serializers.ModelSerializer):
             # То создаём запись о котике без его достижений
             cat = Cat.objects.create(**validated_data)
             return cat
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
 
         # Иначе делаем следующее:
         # Уберём список достижений из словаря validated_data и сохраним его
@@ -45,4 +84,10 @@ class OwnerSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Owner
-        fields = ('first_name', 'last_name', 'cats') 
+        fields = ('first_name', 'last_name', 'cats')
+
+
+class CustomUserSerializer(UserSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'id', 'username', 'first_name', 'last_name')
--- kittygram_plus/settings.py
+++ kittygram_plus/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'cats.apps.CatsConfig',
+    'rest_framework',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,20 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated', 
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+
+SIMPLE_JWT = {
+    # Устанавливаем срок жизни токена
+   'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+   'AUTH_HEADER_TYPES': ('Bearer',),
+} 
